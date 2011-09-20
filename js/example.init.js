fbd0b671ea03a8a931b4bb19c0174412938f0c5c
stephband <stephband@googlemail.com>
2011-09-20 14:36:32 +0100

    Adds app files

---
+++ js/example.init.js
@@ -0,0 +1,16 @@
+// Make aliases for webdoc, app and app.onload.
+window.webdoc = WebDoc;
+window.app = webdoc.App;
+app.onload = app.onLoad;
+
+
+
+console.log(webdoc);
+
+app.onload(function() {
+	console.log('APP Loaded');
+	
+	
+	
+	
+});
